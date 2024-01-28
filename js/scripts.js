92acd73e0c3093e7374d2abef6caabb89e957572
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-01-28 12:29:14 -0300

    evento de alteração do  nome da tarefa - pronto

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@ const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+let oldInputValue;
 
 //Funções
-
-const saveTodo = (text) => {
+const saveTodo = (text) => { //cria a estrutuda da nova tarefa
 
     const todo = document.createElement("div"); //cria uma div
     todo.classList.add("todo"); //adiciona uma class na div
@@ -39,20 +39,45 @@ const saveTodo = (text) => {
 
 };
 
+const toggleForms = () => {//troca a class das divs para mostrar ou ocultar 
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {//altera o h3 da tarefa
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3");
+
+        if(todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        };
+    });
+
+};
+
 //Eventos
-todoForm.addEventListener("submit", e => {
+todoForm.addEventListener("submit", e => {//cria a estrutuda da nova tarefa
     e.preventDefault();
 
     const inputValue = todoInput.value;
 
     if(inputValue) {
         saveTodo(inputValue);
-    }
+    };
+
 });
 
 document.addEventListener("click", e => {
     const targetEl = e.target; //para saber qual elemento foi clicado
     const parentEl = targetEl.closest("div"); //seleciona o elemento "div" mais próximo
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
  
     if(targetEl.classList.contains("finish-todo")) { //verifica se o elemento clicado tem a clase "finish-todo"
         parentEl.classList.toggle("done");//adiciona a class "done" na div pai
@@ -61,6 +86,31 @@ document.addEventListener("click", e => {
 
     if(targetEl.classList.contains("remove-todo")) {
         parentEl.remove(); //remove a div
-    }
+    };
 
+    if(targetEl.classList.contains("edit-todo")) {
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle; //salvando pra poder alterar
+    };
+
+});
+
+cancelEditBtn.addEventListener("click", e => {
+    e.preventDefault();
+    toggleForms();
+
+});
+
+editForm.addEventListener("submit", e => { //edita o h3 da tarefa
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if(editInputValue) {
+        updateTodo(editInputValue);
+    };
+
+    toggleForms();
 });
